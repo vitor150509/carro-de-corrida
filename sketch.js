293b8c9cc254738f369cc220411e57e8dfc8a8bd
vitor150509 <vitor.ribeiro.silva15@escola.pr.gov.br>
2025-04-29 11:52:01 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,56 @@
+function setup() {
+  createCanvas(400, 400);
+}
+ let xJogador1=0;
+ let xJogador2=0;
+function draw() {
+ if (focused == true) {
+   backgroud("green");
+ function ativaJogo(){ 
+ if (focouseed== true)};
+   background("teal");
+   } (else) {}
+   textSize(40); 
+ text("😈",xJogador1,100);
+ text("🥶",xJogador2,200);
+  rect(350,0,10,400);
+ fill("rgb(0,0,0)");
+ if (xJogador1 > 350) {
+  noLoop();
+   text("Jogador 1 venceu!", 50, 200);
+if (xJogador2 > 350) {
+  text("Jogador 2 venceu!", 50, 200);
+  noLoop();
+  }
+}
+function KeyReleased() {
+  if (key === "a" );
+    xJogador1+=random(5);
+}
+ if (key== "s") {
+   xJogador2+=random(5);
+ function ativaJogo(){
+ if (focouseed== true)};
+   background("teal");
+   } (else) {}  
+   textSize(40); 
+ text("😈",xJogador1,100);
+ text("🥶",xJogador2,200);
+}
+go de corrida - aula 6
+por startmarcelo   function desenhalLinhaDeChegada() }
+  {}
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
